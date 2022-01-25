692ee3062cc3dd5bde7739e1ad4b0bddf33e14bb
avrpic8 <avrpic8@gmail.com>
2022-01-25 23:09:00 +0330

    start project

---
+++ android/app/src/main/kotlin/com/smartElectronic/flutter_weather/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.smartElectronic.flutter_weather
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/data/models/weather_data.dart
@@ -0,0 +1,49 @@
+import 'package:flutter_weather/app/data/models/current.dart';
+import 'package:flutter_weather/app/data/models/minutley.dart';
+
+class WeatherData {
+  double? lat;
+  double? lon;
+  String? timezone;
+  int? timezoneOffset;
+  Current? current;
+  List<Minutely>? minutely;
+
+  WeatherData(
+      {this.lat,
+      this.lon,
+      this.timezone,
+      this.timezoneOffset,
+      this.current,
+      this.minutely});
+
+  WeatherData.fromJson(Map<String, dynamic> json) {
+    lat = json['lat'];
+    lon = json['lon'];
+    timezone = json['timezone'];
+    timezoneOffset = json['timezone_offset'];
+    current =
+        json['current'] != null ? Current.fromJson(json['current']) : null;
+    if (json['minutely'] != null) {
+      minutely = <Minutely>[];
+      json['minutely'].forEach((v) {
+        minutely!.add(Minutely.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['lat'] = lat;
+    data['lon'] = lon;
+    data['timezone'] = timezone;
+    data['timezone_offset'] = timezoneOffset;
+    if (current != null) {
+      data['current'] = current!.toJson();
+    }
+    if (minutely != null) {
+      data['minutely'] = minutely!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
