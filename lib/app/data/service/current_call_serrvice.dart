46eb44299990a2d1239ccd96232e2057b05476a1
avrpic8 <avrpic8@gmail.com>
2022-01-26 11:55:49 +0330

    create two service api class

---
+++ lib/app/data/service/current_call_serrvice.dart
@@ -0,0 +1,10 @@
+import 'package:get/get_connect.dart';
+
+class CurrentCallService extends GetConnect{
+
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+  }
+}
---
+++ lib/app/data/service/one_call_service.dart
@@ -0,0 +1,10 @@
+import 'package:get/get_connect.dart';
+
+class OneCallService extends GetConnect{
+
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+  }
+}
